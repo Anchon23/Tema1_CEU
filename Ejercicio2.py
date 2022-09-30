dc45fbca23da2d52a20e440c98d0e02b19d08f9f
Anchon23 <angellopezrouanet@gmail.com>
2022-09-30 13:00:33 +0200

    Mi segundo commit

---
+++ Ejercicio2.py
@@ -0,0 +1,5 @@
+numero_magico = 12345679
+numero_usuario = float(input("Introduce un numero entre el 1 y el 9: "))
+numero_usuario2 = numero_usuario * 9
+numero_magico2 = numero_magico * numero_usuario2
+print(numero_magico2)
