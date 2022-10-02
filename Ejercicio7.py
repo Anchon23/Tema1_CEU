f27aec85abe41939fccb29bcf483f90d41e9933e
Anchon23 <angellopezrouanet@gmail.com>
2022-10-02 21:17:22 +0200

    Mi segundo commit

---
+++ Ejercicio7.py
@@ -0,0 +1,11 @@
+from io import UnsupportedOperation
+
+
+def agregar_una_vez(lista, el):
+    lista = []
+    el = usuario
+    usuario = input("Introduce un elemento: ")
+    if el == lista:
+        print("ValueError")
+    else:
+        lista.append(el)
